73ff472042ab3b7b30b15a7f0bd33283cf122a36
everhopingandwaiting <jackyao713911@gmail.com>
2016-05-07 09:08:30 +0800

    着手doc docx 解析,修复some bug

--- src/main/java/com/jy/tool/ConvertTo.java
+++ src/main/java/com/jy/tool/ConvertTo.java
@@ -3,6 +3,8 @@ package com.jy.tool;
 import com.jy.domain.WordToHtml;
 import com.jy.service.WordToHtmlService;
 import org.apache.poi.hwpf.HWPFDocument;
+import org.apache.poi.hwpf.extractor.Word6Extractor;
+import org.apache.poi.hwpf.extractor.WordExtractor;
 import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,12 +40,35 @@ public class ConvertTo {
     public static String ToHtml(String path) throws Exception {
 //        String content = new StringBuffer().toString();
         if (path.equals("") || path == null) {
-            path = "src/main/resources/test.docx";
+            path = "src/main/resources/test.doc";
         }
         InputStream inputStream = new FileInputStream(new File(path));
-        XWPFDocument document = new XWPFDocument(inputStream);
-        XWPFWordExtractor extractor = new XWPFWordExtractor(document);
-        return extractor.getText();
+        if (path.matches("(.*).doc$")) {
+
+//            HWPFDocument document = new HWPFDocument(inputStream);
+//            return document.getDocumentText();
+            WordExtractor extractor = new WordExtractor(inputStream);
+            System.out.println(extractor.getTextFromPieces());
+            //输出页眉的内容
+            System.out.println("页眉：" + extractor.getHeaderText());
+            //输出页脚的内容
+            System.out.println("页脚：" + extractor.getFooterText());
+            //输出当前word文档的元数据信息，包括作者、文档的修改时间等。
+            System.out.println(extractor.getMetadataTextExtractor().getText());
+            //获取各个段落的文本
+            String paraTexts[] = extractor.getParagraphText();
+            for (int i=0; i<paraTexts.length; i++) {
+                System.out.println("Paragraph " + (i+1) + " : " + paraTexts[i]);
+            }
+            return extractor.getText();
+        } else if (path.matches("(.*).docx$")) {
+
+            XWPFDocument document = new XWPFDocument(inputStream);
+            XWPFWordExtractor extractor = new XWPFWordExtractor(document);
+
+            return extractor.getText();
+        }
+        return "不支持的格式";
     }
 
 }
