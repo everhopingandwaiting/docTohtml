9e2dbd4eebfa55d564031ed44d00b2a0e2cf45e5
everhopingandwaiting <jackyao713911@gmail.com>
2016-05-07 19:41:58 +0800

    doc2Html 添加

---
+++ src/main/java/com/jy/tool/DocToHtml.java
@@ -0,0 +1,109 @@
+package com.jy.tool;
+
+import org.apache.poi.hwpf.HWPFDocument;
+import org.apache.poi.hwpf.converter.PicturesManager;
+import org.apache.poi.hwpf.converter.WordToHtmlConverter;
+import org.apache.poi.hwpf.usermodel.Picture;
+import org.apache.poi.hwpf.usermodel.PictureType;
+import org.apache.poi.xwpf.converter.core.FileURIResolver;
+import org.w3c.dom.Document;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.*;
+import java.util.List;
+
+/**
+ * Created by john on 16-5-7.
+ */
+public class DocToHtml {
+
+    public static void main(String argv[]) {
+        try {
+            convert2Html("", "");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void writeFile(String content, String path) {
+        if (content.equals("") || content == null || path.equals("")) {
+            content = "src/main/resources/test.doc";
+            path = "src/main/resources";
+        }
+        FileOutputStream fos = null;
+        BufferedWriter bw = null;
+        try {
+            File file = new File(path);
+            fos = new FileOutputStream(file);
+            bw = new BufferedWriter(new OutputStreamWriter(fos, "UTF-8"));
+            bw.write(content);
+        } catch (FileNotFoundException fnfe) {
+            fnfe.printStackTrace();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        } finally {
+            try {
+                if (bw != null)
+                    bw.close();
+                if (fos != null)
+                    fos.close();
+            } catch (IOException ie) {
+            }
+        }
+    }
+
+    public static void convert2Html(String fileName, String outPutFile) throws TransformerException, IOException,
+            ParserConfigurationException {
+        if (fileName.equals("") || fileName == null || outPutFile.equals("")) {
+            fileName = "src/main/resources/test.doc";
+            outPutFile = fileName+".html";
+        }
+        HWPFDocument wordDocument = new HWPFDocument(new FileInputStream(fileName));//WordToHtmlUtils.loadDoc(new FileInputStream(inputFile));
+        WordToHtmlConverter wordToHtmlConverter = new WordToHtmlConverter(
+                DocumentBuilderFactory.newInstance().newDocumentBuilder()
+                        .newDocument());
+        wordToHtmlConverter.setPicturesManager(new PicturesManager() {
+            public String savePicture(byte[] content,
+                                      PictureType pictureType, String suggestedName,
+                                      float widthInches, float heightInches) {
+                return "files/img/" + suggestedName;
+            }
+        });
+        wordToHtmlConverter.processDocument(wordDocument);
+        //save pictures
+        List pics = wordDocument.getPicturesTable().getAllPictures();
+        if (pics != null) {
+            for (int i = 0; i < pics.size(); i++) {
+                Picture pic = (Picture) pics.get(i);
+                System.out.println();
+                FileURIResolver picPath = new FileURIResolver(new File(fileName));
+                try {
+                    pic.writeImageContent(new FileOutputStream(picPath
+                            + pic.suggestFullFileName()));
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        Document htmlDocument = wordToHtmlConverter.getDocument();
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        DOMSource domSource = new DOMSource(htmlDocument);
+        StreamResult streamResult = new StreamResult(out);
+
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer serializer = tf.newTransformer();
+        serializer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+        serializer.setOutputProperty(OutputKeys.INDENT, "yes");
+        serializer.setOutputProperty(OutputKeys.METHOD, "html");
+        serializer.transform(domSource, streamResult);
+        out.close();
+        writeFile(new String(out.toByteArray()), outPutFile);
+    }
+}
