052804d40a0ceaf845ceb308c200cb54ddcb04b5
everhopingandwaiting <jackyao713911@gmail.com>
2016-05-07 13:43:51 +0800

    除特殊数学公式显示有问题, 其他OK

--- src/main/java/com/jy/tool/DocxToHtml.java
+++ src/main/java/com/jy/tool/DocxToHtml.java
@@ -1,5 +1,6 @@
 package com.jy.tool;
 
+import org.apache.poi.xwpf.converter.core.BasicURIResolver;
 import org.apache.poi.xwpf.converter.core.FileImageExtractor;
 import org.apache.poi.xwpf.converter.core.FileURIResolver;
 import org.apache.poi.xwpf.converter.xhtml.XHTMLConverter;
@@ -14,33 +15,30 @@ import java.io.*;
 public class DocxToHtml
 
 {
-    public static void doGenerateSysOut(String filepath,String path) throws IOException {
+    public static String doGenerateSysOut(String filepath, String path) throws IOException {
         if (filepath.equals("") || filepath == null || path.equals("")) {
             filepath = "src/main/resources/gongshi.docx";
             path = "src/main/resources";
         }
-        String fileOutName = path+filepath.split(path)[1] + ".html";
+        String fileOutName = path + filepath.split(path)[1] + ".html";
 
         long startTime = System.currentTimeMillis();
 
         XWPFDocument document = new XWPFDocument(new FileInputStream(filepath));
         XHTMLOptions options = XHTMLOptions.create().indent(4);
-        // Extract image
-        File imageFolder = new File(path +"/img" );
+        options.URIResolver(new BasicURIResolver("files/img"));
+        File imageFolder = new File(path + "/files/img");
         options.setExtractor(new FileImageExtractor(imageFolder));
-        // URI resolver
-        options.URIResolver(new FileURIResolver(imageFolder));
 
         File outFile = new File(fileOutName);
         outFile.getParentFile().mkdirs();
         OutputStream out = new FileOutputStream(outFile);
         XHTMLConverter.getInstance().convert(document, out, options);
-
-        System.out.println("Generate " + fileOutName + " with " + (System.currentTimeMillis() - startTime) + " ms.");
+        return fileOutName;
     }
 
     public static void main(String[] args) throws IOException {
-        doGenerateSysOut("","");
+        doGenerateSysOut("", "");
 
     }
 
